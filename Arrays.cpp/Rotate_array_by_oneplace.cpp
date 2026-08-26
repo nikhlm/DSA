af91dbf5607ba8f5b801876d600c5c9200197d28
Nikhil Choudhary <nikhil828893@gmail.com>
2026-08-27 00:01:07 +0530

    rotation

---
+++ Arrays.cpp/Rotate_array_by_oneplace.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    int last = arr[n - 1];
+
+    // Shift elements to the right
+    for (int i = n - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = last;
+
+    // Print array
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
